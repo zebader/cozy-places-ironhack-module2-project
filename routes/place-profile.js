5641869ae560ce466359175387fdd3d46ca0a432
Jesus Cebader <zebader@hotmail.com>
2019-05-07 00:53:41 +0200

    place-card add similar places at bottom

--- routes/place-profile.js
+++ routes/place-profile.js
@@ -6,12 +6,29 @@ const Place = require('../models/place');
 
 router.get("/place-profile", (req, res, next) => {
   const API_id = {
-    API_id:'54246235498ec95dc8bc9dde'
+    //API_id:'5882810014f8f4494f93fa21'
+    API_id:'4b9164f1f964a520d9b833e3'
   }
 
-  Place.findOne(API_id)
-  .then( (place) => res.render('apitest/place-profile', { place } ))
-  .catch( (err) => console.log(err));
+  axios.get(`https://api.foursquare.com/v2/venues/${API_id.API_id}/similar`,{
+    params: {
+      client_id: config.client_id,
+      client_secret: config.client_secret,
+      v: '20180323'
+    }
+  }).then( (response) => {
+    const similar = {
+      similar:response.data.response.similarVenues.items
+    }
+
+    Place.findOne(API_id)
+    .then( (place) => res.render('apitest/place-profile', { place,similar } ))
+    .catch( (err) => console.log(err));
+  })
+  .catch((error) => {
+    next(error)
+  })
+
   });
 
 module.exports = router;
